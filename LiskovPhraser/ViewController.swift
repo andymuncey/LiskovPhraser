78e328399e7e9101630c794856e35ae4b48e3176
andymuncey <andrewmuncey@Andrews-MacBook-Pro.local>
2015-07-14 14:52:11 +0100

    Initial Commit

---
+++ LiskovPhraser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LiskovPhraser
+//
+//  Created by Andrew Muncey on 14/07/2015.
+//  Copyright (c) 2015 University of Chester. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
